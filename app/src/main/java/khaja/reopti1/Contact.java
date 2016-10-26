d18f098fda0136102689f70d32b82fbbbe730493
Khaja Mohiddin Shaik <khajaphysicsatheart@gmail.com>
2016-10-26 14:16:41 +0530

    Added Contact Class for help with Database

---
+++ app/src/main/java/khaja/reopti1/Contact.java
@@ -0,0 +1,67 @@
+package khaja.reopti1;
+
+public class Contact {
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    String name;
+    String number;
+    String state;
+    String operator;
+    int minutes;
+    int seconds;
+
+    public Contact(String name, String number, String state, String operator, int minutes, int seconds){
+        this.name = name;
+        this.number = number;
+        this.state = state;
+        this.operator = operator;
+        this.minutes = minutes;
+        this.seconds =  seconds;
+    }
+}
